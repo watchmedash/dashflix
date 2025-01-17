9de9813a518b279d95ab201f9fe8e78644ebfc2d
watchmedash <mojazer2017@gmail.com>
2025-01-17 05:25:13 +0400

    up

--- noodle.js
+++ noodle.js
@@ -4,22 +4,19 @@ document.addEventListener("DOMContentLoaded", () => {
   noodleOverlay.innerHTML = `
     <div class="noodle-overlay active" id="noodleOverlay">
       <div class="noodle">
-        <img src="https://i.postimg.cc/LXMbpNgr/Screenshot-2025-01-06-153937.png" alt="Landscape">
+        <img src="https://i.postimg.cc/cCZSjMrN/Screenshot-2025-01-17-051945.png" alt="Landscape">
         <div class="content">
-          <!--<h2>Breaking News!</h2>-->
-        <p>Prefer a bigger screen? Visit dashflix.top on your preferred device.</p>
+          <!--<h1>Visit our Website!</h1>-->
+        <p>Visit our website at dashflix.top on your preferred device.</p>
           <button class="close-button" id="closeNoodle">Close</button>
         </div>
       </div>
     </div>
   `;
   document.body.appendChild(noodleOverlay);
-
-  // Get close button and overlay elements
   const closeNoodle = document.getElementById("closeNoodle");
   const overlay = document.getElementById("noodleOverlay");
 
-  // Close the modal when the button is clicked
   closeNoodle.addEventListener("click", () => {
     overlay.classList.remove("active");
   });
