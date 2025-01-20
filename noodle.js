104166c1eed78b3bf96b3eca8f8c872231150475
watchmedash <mojazer2017@gmail.com>
2025-01-20 10:33:24 +0400

    Update noodle.js

--- noodle.js
+++ noodle.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
   noodleOverlay.innerHTML = `
     <div class="noodle-overlay active" id="noodleOverlay">
       <div class="noodle">
-        <img src="https://i.postimg.cc/cCZSjMrN/Screenshot-2025-01-17-051945.png" alt="Landscape">
+        <img src="https://i.postimg.cc/QMX8C14j/Google.png" alt="Dashflix">
         <div class="content">
           <!--<h1>Visit our Website!</h1>-->
-        <p>Visit our website at dashflix.top on your preferred device.</p>
+        <p>Google Dashflix now.</p>
           <button class="close-button" id="closeNoodle">Close</button>
         </div>
       </div>
