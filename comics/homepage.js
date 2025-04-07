b8e1a885291e330862cd2cda572a4b7188c59b1e
watchmedash <mojazer2017@gmail.com>
2025-04-07 08:56:34 +0400

    up

--- comics/homepage.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('figure').show();
-
-    $('.prev-page, .next-page').hide();
-
-    $('.prev-page, .next-page').off('click');
-
-    window.onpopstate = null;
-});
