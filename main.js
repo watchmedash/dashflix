bbc55502bec1118cd7f80c727c7b122b18995c82
watchmedash <mojazer2017@gmail.com>
2025-07-22 11:20:57 +0400

    up

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ function displayMovies(moviesList, append = false) {
     });
 
     const link = document.createElement("a");
-    link.href = `player.html?id=${movie.id}`;
+    link.href = `redirect.html?id=${movie.id}&target=player.html&type=movie`;
     link.target = "_self";
     link.appendChild(image);
 
